ae9ec209d8e7b41aa731040c21269067868d4e6b
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-01-09 21:57:58 +1030

    Added gravity and jumping physics.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,15 @@ public class PlayerMovement : MonoBehaviour
     
     public CharacterController controller;
     public float speed = 4f;
+    public float gravity = -100.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,10 +26,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = 0;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+        Debug.Log(isGrounded);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if(isGrounded == false){
+            velocity.y += gravity * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
     }
 }
