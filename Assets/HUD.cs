995e7ce38155b0cd4f5f1b77309d452f77e2d0a0
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-01-14 22:05:29 +1030

    Updates nirnroot counter on pickup.

--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour{
     public GameObject textUI;
-    
+    public Text nirnrootTextUI;
+    private int nirnrootCounter;
+    int totalNirnroot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nirnrootCounter = 0;
+        totalNirnroot = 1;
+        nirnrootTextUI.text = "Nirnroot: " + nirnrootCounter + "/" + totalNirnroot;
     }
 
     // Update is called once per frame
@@ -23,5 +29,9 @@ public class HUD : MonoBehaviour{
 
     public void CloseMessageText(){
         textUI.SetActive(false);
+    }
+
+    public void UpdateNirnrootCounter(){
+        nirnrootTextUI.text = "Nirnroot: " + ++nirnrootCounter + "/" + totalNirnroot;
     }
 }
