f93a34858612a2a2ba85a6c48e17ea45bbe9b374
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-01-28 22:49:39 +1030

    Added method to handle, display and remove quest start UI.

--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour{
     public GameObject textUI;
     public Text nirnrootTextUI;
+    public Text questStartTextUI;
+
     private int nirnrootCounter;
     int totalNirnroot;
 
@@ -15,6 +17,7 @@ public class HUD : MonoBehaviour{
         nirnrootCounter = 0;
         totalNirnroot = 10;
         nirnrootTextUI.text = "Nirnroot: " + nirnrootCounter + "/" + totalNirnroot;
+        StartCoroutine(startQuest("QUEST STARTED: NIRNROOT COLLECTOR"));
     }
 
     // Update is called once per frame
@@ -33,5 +36,12 @@ public class HUD : MonoBehaviour{
 
     public void UpdateNirnrootCounter(){
         nirnrootTextUI.text = "Nirnroot: " + ++nirnrootCounter + "/" + totalNirnroot;
+    }
+
+    IEnumerator startQuest(string text){
+        questStartTextUI.text = text;
+        questStartTextUI.enabled = true;
+        yield return new WaitForSeconds(5);
+        questStartTextUI.enabled = false;
     }
 }
