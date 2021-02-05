c031d418a78298d10589308e3914fdf247283bc8
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-02-05 23:36:56 +1030

    Added start of ai script.

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent agent;
+
+
+    private void Awake(){
+        player = GameObject.Find("First Person Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
