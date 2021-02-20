49e6111d92ce43afa3bb0a6c49b9a3ce3935376e
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-02-20 19:50:06 +1030

    Added enemy entity and AI script.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,12 +5,25 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public float lookRadius = 100f;
     public Transform player;
-    public NavMeshAgent agent;
-
-
-    private void Awake(){
-        player = GameObject.Find("First Person Player").transform;
+    NavMeshAgent agent;
+    
+    void Start(){
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if(distance <= lookRadius){
+            agent.SetDestination(player.position);
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
